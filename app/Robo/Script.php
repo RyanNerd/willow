85b3f9756a59b14afe667f30444c4cf33a28e0bf
Ryan Jentzsch <ryan.jentzsch@gmail.com>
2019-05-03 23:44:54 -0600

    Symlink post package install (attempt 5)

--- app/Robo/Script.php
+++ app/Robo/Script.php
@@ -7,8 +7,16 @@ class Script
 {
     public static function postPackageInstall($event)
     {
+        $absolutePath = __DIR__ . '../../vendor/consolidation/robo/robo';
         $path = __DIR__ . '/../../vendor/bin/robo';
-        echo $path;
-        symlink(__DIR__ . '/../../vendor/bin/robo', 'willow');
+        if (!file_exists($path)) {
+            if (file_exists($absolutePath)) {
+                symlink($absolutePath, 'willow');
+            } else {
+                echo PHP_EOL . 'NOW WHAT?!?' . PHP_EOL;
+            }
+        } else {
+            symlink(__DIR__ . '/../../vendor/bin/robo', 'willow');
+        }
     }
 }
