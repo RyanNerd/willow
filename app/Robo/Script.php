18a00dcaec46767e2fc8aacb18f6e03582ae0a8b
Ryan Jentzsch <ryan.jentzsch@gmail.com>
2019-05-03 23:23:48 -0600

    Symlink post package install (attempt 4)

--- app/Robo/Script.php
+++ app/Robo/Script.php
@@ -7,6 +7,8 @@ class Script
 {
     public static function postPackageInstall($event)
     {
+        $path = __DIR__ . '/../../vendor/bin/robo';
+        echo $path;
         symlink(__DIR__ . '/../../vendor/bin/robo', 'willow');
     }
 }
