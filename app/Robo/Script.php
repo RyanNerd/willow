ec0b0bc261341fc67bedc18392f010b36afcc6e5
Ryan Jentzsch <ryan.jentzsch@gmail.com>
2019-05-04 01:51:39 -0600

    Some fun with Climate and composer

--- app/Robo/Script.php
+++ app/Robo/Script.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Willow\Robo;
 
+use League\CLImate\CLImate;
+
 /**
  * Composer script
  */
@@ -19,6 +21,26 @@ class Script
                 // Create the willow symlink file
                 symlink(__DIR__ . '/../../vendor/bin/robo', 'willow');
             }
+        }
+    }
+
+    // Will this fire from a package install?
+    public static function postInstallCmd($event)
+    {
+        $cli = new CLImate();
+        $cli->forceAnsiOn();
+        $cli->addArt(__DIR__);
+        $cli->green()->border('*', 45);
+        $cli->bold()->lightGreen()->animation('willow')->speed(200)->enterFrom('left');
+        $cli->backgroundGreen()->lightGray(' 🌳 https://github.com/RyanNerd/willow 🌳');
+        $cli->green()->border('*', 45);
+        $cli->bold()->white()->inline('Thanks for installing ');
+        $cli->bold()->lightGreen()->inline('Willow');
+        $cli->bold()->white('!');
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $cli->bold()->white('To get started type: willow help');
+        } else {
+            $cli->bold()->white('To get started type: ./willow help');
         }
     }
 }
