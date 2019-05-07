50eb2372aeea6a1513394472c0c9c32f3c15e441
Ryan Jentzsch <ryan.jentzsch@gmail.com>
2019-05-07 14:02:57 -0600

    Fix JsonBodyParserMiddleware JSON_ERROR_NONE comparison

--- app/Middleware/JsonBodyParserMiddleware.php
+++ app/Middleware/JsonBodyParserMiddleware.php
@@ -21,7 +21,7 @@ class JsonBodyParserMiddleware implements MiddlewareInterface
 
         if (strstr($contentType, 'application/json')) {
             $contents = json_decode(file_get_contents('php://input'), true);
-            if (json_last_error() !== JSON_ERROR_NONE) {
+            if (json_last_error() === JSON_ERROR_NONE) {
                 $request = $request->withParsedBody($contents);
             }
         }
