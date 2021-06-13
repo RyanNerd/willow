39cad7329d323ae7d13767114759e182dc3ac5f5
ryan <ryan.jentzsch@gmail.com>
2021-06-13 15:56:51 -0600

    Refactor Sample command
    - SampleCommands.php extracted from WillowCommands.php as a standalone task command.
    - `eject` removes SampleCommands.php

--- app/Robo/Plugin/Commands/WillowCommands.php
+++ app/Robo/Plugin/Commands/WillowCommands.php
@@ -15,19 +15,6 @@ class WillowCommands extends RoboBase
     }
 
     /**
-     * Launch the built-in PHP server and load the Sample in a web browser
-     */
-    final public function sample(): void {
-        $this->taskServer(8088)
-            ->host('127.0.0.1')
-            ->dir(__DIR__ . '/../../../../public')
-            ->background()
-            ->run();
-        $this->taskOpenBrowser('http://localhost:8088/v1/sample/Hello-World')->run();
-        sleep(15);
-    }
-
-    /**
      * Show Willow's fancy banner
      */
     final public function banner(): void {
